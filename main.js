696739c13d9805658dfe22c044937085e1c4614b
Maggievira87 <margarethjvr87@gmail.com>
2025-11-05 12:42:47 -0500

    Desarrollo actividad

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
-import { PI, IVA } from './constantes.js';
+import { PI, IVA, DIAS_SEMANA, DESCUENTO } from './constantes.js';
+
+import { sumar, restar, multiplicar, areaCirculo, calcularTotal, aplicarImpuestos, aplicarDescuento, aplicarDescuentoPorRol } from './operaciones-matematicas.js';
+
+import { ROLES } from './usuariosConstantes.js';
+
+import { crearUsuario } from './usuarios.js';
 
 const radio = 5;
 const areaCirculo = PI * (radio ** 2);
-console.log(`Área del círculo con radio ${radio}: ${areaCirculo}`);
+console.log(`El área del círculo con radio ${radio} es: ${areaCirculo}`);
 
-const precio = 100;
-const precioConIVA = precio * (1 + IVA);
-console.log(`Precio con IVA: ${precioConIVA}`);
+const precioBase = 100;
+const precioConIVA = precioBase + (precioBase * IVA);
+console.log(`El precio con IVA es: ${precioConIVA}`);
+
+console.log(`El tercer día de la semana es: ${DIAS_SEMANA[2]}`);
---
+++ operaciones-matematicas.js
@@ -0,0 +1,31 @@
+import { PI } from "./constantes.js";
+
+export function sumar(a, b) {
+
+  return a + b;
+
+}
+
+// Función para restar dos números
+
+export function restar(a, b) {
+
+  return a - b;
+
+}
+
+// Función para multiplicar dos números
+
+export function multiplicar(a, b) {
+
+  return a * b;
+
+}
+
+// Función para calcular el área de un círculo usando PI
+
+export function areaCirculo(radio) {
+
+  return PI
+
+}
